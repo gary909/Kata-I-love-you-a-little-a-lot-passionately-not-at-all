690b5d861c5bdc3570409b26f0562be64fd11fb9
gary <gary.white@lodgit.com>
2022-01-24 13:34:35 +0100

    code working

--- index.js
+++ index.js
@@ -1,6 +1,35 @@
+// function howMuchILoveYou(nbPetals) {
+//     // your code
+//     if (nbPetals == 7) {
+//         return "I love you";
+//     } else if (nbPetals == 6) {
+//         return "not at all";
+//     } else if (nbPetals == 5) {
+//         return "x5";
+//     } else if (nbPetals == 4) {
+//         return "x4";
+//     } else if (nbPetals == 3) {
+//         return "a lot";
+//     } else if (nbPetals == 2) {
+//         return "x2";
+//     } else if (nbPetals == 1) {
+//         return "x1";
+//     } else {
+//         return "x0t";
+//     }
+// }
+
 function howMuchILoveYou(nbPetals) {
-    // your code
-}
+    let phrase = {    
+      0: "not at all",
+      1: "I love you",
+      2: "a little",
+      3: "a lot",
+      4: "passionately",
+      5: "madly"
+    }
+    return phrase[nbPetals%6]
+  }
 
 console.log(howMuchILoveYou(7)); // return "I love you"
 console.log(howMuchILoveYou(3)); // return "a lot"
