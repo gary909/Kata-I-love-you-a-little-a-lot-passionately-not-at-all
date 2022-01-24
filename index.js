22f9e70850bd9522078b2bd5fda93243780fcf82
gary <gary.white@lodgit.com>
2022-01-24 12:53:12 +0100

    template

---
+++ index.js
@@ -0,0 +1,7 @@
+function howMuchILoveYou(nbPetals) {
+    // your code
+}
+
+console.log(howMuchILoveYou(7)); // return "I love you"
+console.log(howMuchILoveYou(3)); // return "a lot"
+console.log(howMuchILoveYou(6)); // return "not at all"
